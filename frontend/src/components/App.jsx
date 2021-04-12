381b7bb141e144cc3ec39e15bf71672057ddba15
pekka-dev <prashant.garg.1901@gmail.com>
2021-04-12 23:34:56 +0530

    Change theme  - Primary color => grey  - Secondary color => white

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { grey } from '@material-ui/core/colors';
 import Home from './Home';
-import SignUp from './SignUp';
-import LogIn from './LogIn';
+import SignUp from './auths/SignUp';
+import LogIn from './auths/LogIn';
+import ForgotPassword from './auths/ForgotPassword';
 import { AuthProvider } from '../contexts/AuthContext';
-import PrivateRoute from './PrivateRoute';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: grey[900],
+        },
+        secondary: {
+            main: '#ffffff',
+        },
+    },
+});
 
 function App() {
     return (
         <BrowserRouter>
-            <AuthProvider>
-                <Switch>
-                    <PrivateRoute exact path="/" component={Home} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/login" component={LogIn} />
-                </Switch>
-            </AuthProvider>
+            <ThemeProvider theme={theme}>
+                <AuthProvider>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/signup" component={SignUp} />
+                        <Route path="/login" component={LogIn} />
+                        <Route path="/forgot-password" component={ForgotPassword} />
+                    </Switch>
+                </AuthProvider>
+            </ThemeProvider>
         </BrowserRouter>
     );
 }
