b90a83331f9f66a681fb25c3708a5d0f6cefceed
pekka-dev <prashant.garg.1901@gmail.com>
2021-04-12 23:24:32 +0530

    Add loading state in AuthContext.js  - Which avoid startup "currentUser is null" error

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import { useHistory } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link, useHistory } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { CssBaseline } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: 0,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 export default function Home() {
+    const classes = useStyles();
+
     const history = useHistory();
     const { signOut, currentUser } = useAuth();
 
@@ -14,11 +37,32 @@ export default function Home() {
     }
 
     return (
-        <div>
-            {currentUser.email1}
-            <Button variant="contained" color="primary" onClick={handleSignOut}>
-                Sign Out
-            </Button>
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="secondary" variant="contained" component={Link} to="/login">
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            <Container component="main" maxWidth="xl">
+                <Paper>{JSON.stringify(currentUser)}</Paper>
+                <Button variant="contained" color="primary" onClick={handleSignOut}>
+                    Sign Out
+                </Button>
+            </Container>
         </div>
     );
 }
--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -9,6 +9,7 @@ export function useAuth() {
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState({});
+    const [loading, setLoading] = useState(true);
 
     function signUp(email, password) {
         return auth.createUserWithEmailAndPassword(email, password);
@@ -25,6 +26,7 @@ export const AuthProvider = ({ children }) => {
     useEffect(() => {
         return auth.onAuthStateChanged((user) => {
             setCurrentUser(user);
+            setLoading(false);
         });
     }, []);
 
@@ -34,5 +36,5 @@ export const AuthProvider = ({ children }) => {
         signUp,
         signOut,
     };
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
 };
